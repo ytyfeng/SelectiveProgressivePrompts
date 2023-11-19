0f09642bd0ac9e5df92c2317cf0e3b57086cc5fb
ytyfeng <yingtaif@gmail.com>
2023-11-19 03:34:30 +0000

    bash script for experiments

---
+++ T5_codebase/experiment.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# similarity_threshold = 0
+python train_t5_cl.py --task_list imdb cb sst2 yelp mrpc qqp dbpedia rte \
+--select_k_per_class 1000 \
+--lr 0.3 --num_epochs 10 \
+--freeze_weights 1 --prefix_len 10 \
+--model_name t5-large --early_stopping 1 \
+--save_name T5_experiment_sim_0 --save_dir save_dir 
+--batch_size 4 --similarity_threshold 0 > sim0_10epochs.out 
+
+
+# similarity_threshold = 0.7
+python train_t5_cl.py --task_list imdb cb sst2 yelp mrpc qqp dbpedia rte \
+--select_k_per_class 1000 \
+--lr 0.3 --num_epochs 10 \
+--freeze_weights 1 --prefix_len 10 \
+--model_name t5-large --early_stopping 1 \
+--save_name T5_experiment_sim_07 --save_dir save_dir 
+--batch_size 4 --similarity_threshold 0.7 > sim07_10epochs.out
+
+
+
