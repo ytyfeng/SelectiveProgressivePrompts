softmax fix

T5_codebase/t5_continual.py
@@ -404,7 +404,7 @@ class T5ContinualLearner:
         return tasks_data_dict
 
     # x is a vec
-    def softmax(x):
+    def softmax(self, x):
         exp_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
         return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
     