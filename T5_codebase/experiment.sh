99405a021d8d5cc43db47a33030d690cc25ed9ca
Ty Feng <ty@radiancelabs.com>
2023-11-23 14:56:06 -0800

    bug fixes

--- T5_codebase/experiment.sh
+++ T5_codebase/experiment.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # similarity_threshold = 0
-python train_t5_cl.py --task_list imdb cb sst2 mrpc \
---select_k_per_class 20 \
---lr 0.3 --num_epochs 3 \
---freeze_weights 1 --prefix_len 10 \
---model_name t5-large --early_stopping 1 \
---save_name T5_experiment_sim_0 --save_dir save_dir \
---batch_size 4 --similarity_threshold 0 > sim0_10epochs.txt 
+# python train_t5_cl.py --task_list imdb cb sst2 mrpc \
+# --select_k_per_class 20 \
+# --lr 0.3 --num_epochs 3 \
+# --freeze_weights 1 --prefix_len 10 \
+# --model_name t5-large --early_stopping 1 \
+# --save_name T5_experiment_sim_0 --save_dir save_dir \
+# --batch_size 4 --similarity_threshold 0 > sim0_10epochs.txt 
 
 
 # similarity_threshold = 0.7
